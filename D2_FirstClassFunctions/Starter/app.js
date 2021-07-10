6fdf16015f14b5a04358094aef436708792b89fc
Soumya Joy <soumyaedappilly@gmail.com>
2021-07-11 02:38:21 +0530

    First class functions

---
+++ D2_FirstClassFunctions/Starter/app.js
@@ -0,0 +1,26 @@
+// Your Javascript Code Goes Here
+// function statement
+function greet(){
+    console.log('hi');
+}
+greet();
+
+// functions are first-class
+function logGreeting(fn){
+    fn();
+}
+logGreeting(greet);
+
+// function expression
+var greetMe = function(){
+    console.log('Hi from Krystal!!');
+}
+greetMe();
+
+// it's first class
+logGreeting(greetMe);
+
+// use a function expression on the fly
+logGreeting(function(){
+    console.log('Happy to have you here');
+})
