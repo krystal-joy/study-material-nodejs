efa90737e4db2413258b56755ae04e43cf9a267d
Soumya Joy <soumyaedappilly@gmail.com>
2021-07-13 07:08:01 +0530

    Template Literals

---
+++ E7_ES6TemplateStrings/Starter/app.js
@@ -0,0 +1,12 @@
+var firstname = 'Krystal';
+var lastname = 'Joy';
+
+// Normal way 
+
+var greet1 = 'Hello, ' + firstname + ' ' + lastname;
+
+// Template Literals
+var greet2 = `Hello, ${firstname} ${lastname}`;
+
+console.log(greet1);
+console.log(greet2);
